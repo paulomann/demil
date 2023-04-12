52dc2400a29476df4e47430f2f2229c8db6fc261
Arthur Bittencourt Vasconcelos <arthurbittencourt@dgxnvidia.ic.uff.br>
2023-04-11 22:27:52 -0300

    centralized_bert_utils_implementation

--- scripts/bert_classifier.py
+++ scripts/bert_classifier.py
@@ -19,8 +19,10 @@ from datasets import Dataset
 
 from transformers import AutoTokenizer, DataCollatorWithPadding
 from transformers import AutoModelForSequenceClassification
-from transformers import AdamW
+from transformers import AdamW, Adafactor
 from evaluate import load
+
+import bert_utils
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -29,6 +31,7 @@ def get_args():
     parser.add_argument("--gpu", type=int, default=7, help="wich device this client will use")
     parser.add_argument("--checkpoint", type=str, default="bert-base-uncased", help="what checkpoint to use, leave blank for bert-base-uncased")
     parser.add_argument("--epochs", type=int, default=1, help="how many epochs to run on this client")
+    parser.add_argument("--experiment_name", type=str, required=True)
 
     args = parser.parse_args()
     return args
@@ -38,82 +41,14 @@ args = get_args()
 DEVICE = torch.device(f"cuda:{args.gpu}")
 CHECKPOINT = args.checkpoint
 EPOCHS = args.epochs
-CSV_SAVE_DIR = f'/home/arthurbittencourt/depression-demil/demil/results/centralized_model.csv' 
+LOGGER = bert_utils.ExperimentLogger(args.experiment_name) 
 
-DATASET_DIR = "/home/arthurbittencourt/depression-demil/demil/data/eRisk2021/"
-
-def dataset_prep(ds):
-    ds.bdi[ds.bdi < 20] = 0
-    ds.bdi[ds.bdi >= 20] = 1
-
-    ds['text'] = ds.caption.fillna('')
-    ds['text'] = ds.text.str.replace(r"http\S+", "HTTP")
-    ds['text'] = ds.text.str.replace(r"@\w*", "USER")
-
-    ds = ds.rename(columns={'bdi':'label'})
-
-    ds = ds[['text', 'label']]
-
-    return ds
-
-def load_data():
-    """Load eRisk2021 data (training and eval)"""
-    df_train = pd.read_csv(DATASET_DIR + f'train.csv')
-    df_test = pd.read_csv(DATASET_DIR + f'test.csv')
-    df_val = pd.read_csv(DATASET_DIR + f'val.csv')
-
-    df_train = dataset_prep(df_train)
-    df_test = dataset_prep(df_test)
-    df_val = dataset_prep(df_val)
-
-    ds_train = Dataset.from_pandas(df_train)
-    ds_test = Dataset.from_pandas(df_test)
-    ds_val = Dataset.from_pandas(df_val)
-
-    tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
-
-    def tokenize_function(examples):
-        return tokenizer(examples["text"], truncation=True)
-
-    ds_train = ds_train.map(tokenize_function, batched=True)
-    ds_test = ds_test.map(tokenize_function, batched=True)
-    ds_val = ds_val.map(tokenize_function, batched=True)
-
-    ds_train = ds_train.remove_columns("text")
-    ds_test = ds_test.remove_columns("text")
-    ds_val = ds_val.remove_columns("text")
-
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-    BATCH_SIZE = 6
-    
-    trainloader = DataLoader(
-        ds_train,
-        shuffle=True,
-        batch_size=BATCH_SIZE,
-        collate_fn=data_collator,
-    )
-
-    testloader = DataLoader(
-        ds_test, 
-        batch_size=BATCH_SIZE, 
-        collate_fn=data_collator
-    )
-
-    valloader = DataLoader(
-        ds_val, 
-        batch_size=BATCH_SIZE, 
-        collate_fn=data_collator
-    )
-
-    return trainloader, testloader, valloader
-
-
-def train(net, trainloader, epochs):
-    optimizer = AdamW(net.parameters(), lr=1e-5)
+def train(net, trainloader, valloader, epochs, lr, logger=LOGGER):
+    optimizer = AdamW(net.parameters(), lr=lr)  # Adafactor(net.parameters(), warmup_init=True) 
     net.train()
     for i in range(epochs):
         for batch in trainloader:
+            if 'username' in batch: batch.pop('username')
             batch = {k: v.to(DEVICE) for k, v in batch.items()}
             outputs = net(**batch)
             loss = outputs.loss
@@ -121,69 +56,29 @@ def train(net, trainloader, epochs):
             optimizer.step()
             optimizer.zero_grad()
 
-        print(f"Round {i+1} Loss: {loss}")
+        metrics = bert_utils.test(net, valloader, DEVICE, logger=LOGGER, source=f"Epoch {i+1}")
+        metrics['train_loss'] = loss.item()   
 
-
-def test(net, valloader):
-    accuracy = load("accuracy")
-    precision = load("precision")
-    recall = load("recall")
-    f1 = load("f1")
-
-    loss = 0
-    net.eval()
-
-    for batch in valloader:
-        batch = {k: v.to(DEVICE) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = net(**batch)
-        logits = outputs.logits
-        loss += outputs.loss.item()
-        predictions = torch.argmax(logits, dim=-1)
-
-        accuracy.add_batch(predictions=predictions, references=batch["labels"])
-        precision.add_batch(predictions=predictions, references=batch["labels"])
-        recall.add_batch(predictions=predictions, references=batch["labels"])
-        f1.add_batch(predictions=predictions, references=batch["labels"])
-
-
-    valloader.dataset
-    loss /= len(valloader.dataset)
-    metrics = {
-        "accuracy":accuracy.compute()["accuracy"],
-        "precision":precision.compute()["precision"],
-        "recall":recall.compute()["recall"],
-        "f1":f1.compute()["f1"],
-    }
-    return loss, metrics
-    
-def append_result(loss, metrics):
-    df = pd.DataFrame(metrics)
-    df['loss'] = loss
-    df.to_csv(CSV_SAVE_DIR, decimal=',', mode='a', header=(not os.path.exists(CSV_SAVE_DIR)))
+        print(f"Epoch {i+1}: {metrics}")
 
 def main():
 
-    net = AutoModelForSequenceClassification.from_pretrained(
-        CHECKPOINT, num_labels=2
-    ).to(DEVICE)
+    net = AutoModelForSequenceClassification.from_pretrained(CHECKPOINT, num_labels=2).to(DEVICE)
 
-    trainloader, _, valloader = load_data()
+    tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+    trainloader, testloader, valloader = bert_utils.load_data(dataset='eRisk2021', partition=False, tokenizer=tokenizer)
 
-    
-    # Start Training
-    print("Sarting Training...'")
-    train(net, trainloader, EPOCHS)
-    
-    results = test(net, valloader)
-    print("Results:", results)
+    # Log baseline
+    baseline_metrics = bert_utils.test(net, testloader, DEVICE, logger=LOGGER, source="Baseline")
 
-    '''
-    print(f"WRITING WEIGHTS FOR SEED {seed}")
-    weights = [val.cpu().numpy() for _, val in model.state_dict().items()]
-    with open("weights.bin", "wb") as handle:
-        pk.dump(weights, handle, protocol=pk.HIGHEST_PROTOCOL)
-    '''
+    # Start Training, three times incrementing the learning rate
+    print("Sarting Training...")
+    train(net, trainloader, valloader, EPOCHS, 1e-5)
+
+    # Final results with testloader
+    results = bert_utils.test(net, testloader, DEVICE, logger=LOGGER, source="Final Results") 
+    print(f"Results: {results}")
+    LOGGER.log_experiment()
 
 
 if __name__ == "__main__":
